5278de6828143531bd0b02e3a0730d63df6ccf4e
Ruichao C <ruichao.private@gmail.com>
2021-02-19 20:48:48 +0000

    Fixed

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const readline = require('readline-sync');
 
-let name = readline.question("What is the PlaceId? (Don't use ctrl+v use right click and paste, idk why it outputs some random stuff) .");
+let PID = readline.question("What is the PlaceId? (Don't use ctrl+v use right click and paste, idk why it outputs some random stuff) .");
 
-console.log("OK, allow this to run, searching through " + name);
+console.log("OK, allow this to run, searching through " + PID);
 
 const Hexanium = require('hexanium')
 
 var Finder = new Hexanium('')
 
-var PlaceId = name
+var PlaceId = PID
 
 
 async function Start() {
     let GUID = await Finder.FindServer(PlaceId)
     console.log(`Roblox.GameLauncher.joinGameInstance(${PlaceId}, "${GUID}")`)
 console.log("\n")
-console.log("OK, Now that it is done it should have output this: Roblox.GameLauncher.joinGameInstance("+ name +","+ GUID +"). So, now go to the roblox game, press F12 and paste it in. You should be prompted to open roblox, or it might do it for you. You are now done! GLHF")
+console.log("OK, Now that it is done it should have output this: Roblox.GameLauncher.joinGameInstance("+ PID +","+ GUID +"). So, now go to the roblox game, press F12 and paste it in. You should be prompted to open roblox, or it might do it for you. You are now done! GLHF")
 }
 
 Start()
